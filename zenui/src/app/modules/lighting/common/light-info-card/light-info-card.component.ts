9af1ca8f1baca8dbf5a793c70eeef1a2328e09b5
Gabriel Vande Hei <gfvandehei@gmail.com>
2020-11-16 18:36:42 -0500

    canvas displays now, but scaling and position is off

--- zenui/src/app/modules/lighting/common/light-info-card/light-info-card.component.ts
+++ zenui/src/app/modules/lighting/common/light-info-card/light-info-card.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-light-info-card',
   templateUrl: './light-info-card.component.html',
   styleUrls: ['./light-info-card.component.css']
 })
-export class LightInfoCardComponent implements OnInit {
+export class LightInfoCardComponent implements AfterViewInit {
   @Input("name") name;
   @Input("address") address;
   @Input("grid") grid: Array<Array<{
@@ -13,25 +13,45 @@ export class LightInfoCardComponent implements OnInit {
     r: number,
     g: number,
     b: number
-  }>>;
+  } | 0>>;
 
   @ViewChild("gridcanvas") canvas: ElementRef;
   private gridcanvas: HTMLCanvasElement;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.gridcanvas = this.canvas.nativeElement;
+    this.drawGridOnCanvas();
   }
 
   drawGridOnCanvas(){
+    let ctx = this.gridcanvas.getContext("2d");
+
     let canvasHeight = this.gridcanvas.height;
     let canvasWidth = this.gridcanvas.width;
 
     let gridHeight = this.grid.length;
     let gridWidth = this.grid[0].length;
 
-
+    let scalingFactorX = canvasHeight/gridHeight;
+    let scalingFactorY = canvasWidth/gridWidth;
+    console.log(scalingFactorY, scalingFactorX);
+    for(let i=0; i<gridHeight; i++){
+      for(let u=0; u<gridWidth; u++){
+        ctx.beginPath();
+        ctx.rect(i*scalingFactorX, u*scalingFactorY, scalingFactorX, scalingFactorY);
+        if(this.grid[i][u] != 0){
+          let r = (this.grid[i][u] as any).r;
+          let g = (this.grid[i][u] as any).g;
+          let b = (this.grid[i][u] as any).b;
+          ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
+        } else{
+          ctx.fillStyle = 'white';
+        }
+        ctx.fill();
+      }
+    }
   }
 
 }
--- zenui/src/app/modules/lighting/home/home.component.ts
+++ zenui/src/app/modules/lighting/home/home.component.ts
@@ -9,9 +9,13 @@ export class HomeComponent implements OnInit {
 
   private lights: Array<any> = [];
   public selectedLights: Array<any> = [
-    {name: "asdasd", address: "123"},
-    {name: "asdasd", address: "123"},
-    {name: "asdasd", address: "123"}
+    {name: "asdasd", address: "123", grid: [
+      [{index: 1, r: 255, g: 0, b:0}, 0, 0],
+      [0, {index: 1, r: 255, g: 0, b:0}, 0],
+      [0, 0, {index: 1, r: 255, g: 0, b:0}]
+    ]},
+    {name: "asdasd", address: "123", grid: [[{index: 1, r: 255, g: 255, b:255}]]},
+    {name: "asdasd", address: "123", grid: [[{index: 1, r: 255, g: 255, b:255}]]}
   ];
 
   constructor() { }
